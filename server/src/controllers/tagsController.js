2368def57fd0d0911575d126fabac1f880746816
JSom46 <96313843+JSom46@users.noreply.github.com>
2023-04-25 19:38:17 +0200

    separeted elements and tags endpoints into their own files

---
+++ server/src/controllers/tagsController.js
@@ -0,0 +1,20 @@
+import Express from "express";
+import { getTags } from "../utils/getTags.js";
+import { validateTagsQueryParams } from "../utils/validateTagsQueryParams.js";
+
+const router = Express.Router();
+
+router.get("/:area/:group", async (req, res) => {
+    if(!validateTagsQueryParams(req.params.area, req.params.group)) return res.sendStatus(400);
+
+    try{
+        const tags = await getTags(req.params.area, req.params.group);
+        return res.status(200).send({tags: tags});
+    }
+    catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+});
+
+export const tagsController = router;
--- server/src/main.js
+++ server/src/main.js
@@ -1,17 +1,13 @@
 import {} from "dotenv/config";
 import Express from "express";
-
-import { Area } from "./utils/area.js";
-import { getElements } from "./utils/getElements.js";
 import { elementsController } from "./controllers/elementsController.js";
-
-//console.log(await getElements([{vals: [{area: Area.artist, val: 'ter', negate: false}, {area: Area.language, val: 'english', negate: false}, {area: Area.series, val: 'pokemon', negate: true}]}, {vals: [{area: Area.series, val: 'original', negate: false}]}]));
-//console.log(await getElements([{vals: [{area: Area.artist, val: 'ter', negate: false}, {area: Area.language, val: 'english', negate: false}, {area: Area.series, val: 'pokemon', negate: true}]}, {vals: [{area: Area.artist, val: 'wakadori nikomi', negate: false}, {area: Area.language, val: 'english', negate: true}]}]));
+import { tagsController } from "./controllers/tagsController.js";
 
 const app = Express();
 
 app.use(Express.json());
-app.use(elementsController);
+app.use("/elements", elementsController);
+app.use("/tags", tagsController);
 
 app.listen(process.env.SERVER_PORT || 7312, () => {
     console.log(`Server listening on port ${process.env.SERVER_PORT || 7312}.`);
